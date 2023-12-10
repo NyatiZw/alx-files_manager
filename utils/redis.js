1daa5c556339b2ee8effbb6969e432e7e5a04f68
NyatiZw <appsmaid19@gmail.com>
2023-12-09 21:58:40 -0800

    initial commit

---
+++ utils/redis.js
@@ -0,0 +1,60 @@
+// utils/redis.js
+
+const redis = require('redis');
+
+class RedisClient {
+	constructor() {
+		this.client = redis.createClient();
+
+		// Display any errors in the console
+		this.client.on('error', (err) => {
+			console.error(`Redis Client Error: ${err}`);
+		});
+	}
+
+	isAlive() {
+		// Check if the connection to Redis is successful
+		return this.client.connected;
+	}
+
+	async get(key) {
+		return new Promise((resolve, reject) => {
+			this.client.get(key, (err, reply) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(reply);
+				}
+			});
+		});
+	}
+
+	async set(key, value, duration) {
+		return new Promise((resolve, reject) => {
+			this.client.setex(key, duration, value, (err) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve('OK');
+				}
+			});
+		});
+	}
+
+	async del(key) {
+		return new Prommise((resolve, reject) => {
+			this.client.del(key, (err, reply) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(reply);
+				}
+			});
+		});
+	}
+}
+
+
+// Create and export an instance of RedisClient
+const redisClient = new RedisClient();
+module.exports = redisClient;
