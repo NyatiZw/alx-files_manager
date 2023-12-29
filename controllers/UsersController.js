2aa8c0171a40c6dce452313fba9cfeba730c262c
NyatiZw <appsmaid19@gmail.com>
2023-12-29 10:26:31 -0800

    update

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,32 +1,63 @@
-const redisClient = require('../redisClient');
-const User = require('../models(User');
+import { ObjecrId } from 'mongodb';
+import sha1 from 'sha1';
+import Queue from 'bull';
+import dbClient from '../utils/db';
+import userUtils from '../utils/user';
 
-module.exports = {
-	getMe: async (req, res) => {
+const userQueue = new Queue('userQueue');
+
+class UserController {
+	static async postNew(req, res) {
+		const { email, password } = request.body;
+
+		if (!email) return res.status(400).send({ error: 'Missing email' });
+
+		if (!password) { return res.status(400).send({ error: 'Missing password' }); }
+
+		const emailExists = await dbClient.usersCollection.findOne({ email });
+
+		if (emailExists) { return res.status(400).send({ error: 'Already exists' }); }
+
+		const sha1Password = sha1(password);
+
+		let result;
 		try {
-			const token = req.headers['x-token'];
-
-			if (!token) {
-				return res.status(401).json({ error: 'Unauthorized' });
-			}
-
-			const key = `auth_${token}`;
-			const userId = await redisClient.get(key);
-
-			if (!userId) {
-				return res.status(401).json({ error: 'Unauthorized' });
-			}
-
-			const user = await User.findById(userId, { email: 1, _id: 1 });
-
-			if (!user) {
-				return res.status(401).json({ error: 'Unauthorized' });
-			}
-
-			return res.json(user);
-		} catch (error) {
-			console.error(error);
-			return res.status(500).json({ error: 'Internal Server Error' });
+			result = await dbClient.usersCollection.insertOne({
+				email,
+				password: sha1Password,
+			});
+		} catch (err) {
+			await userQueue.add({});
+			return res.status(500).send({ error: 'Error creating user.' });
 		}
-	},
-};
+
+		const user = {
+			id: result.insertedId,
+			email,
+		};
+
+		await userQueue.add({
+			userId: result.insertedId.toString(),
+		});
+
+		return res.status(201).send(user);
+	}
+
+	static async.getMe(req, res) {
+		const { userId } = await userUtils.getUserIdAndKey(req);
+
+		cost user = await userUtils.getUser({
+			_id: ObjectId(userId),
+		});
+
+		if (!user) return res.status(401).send({ error: 'Unauthorized' });
+
+		const processedUser = { id: user._id, ..user };
+		delete processedUser._id;
+		delet processedUser.password;
+
+		return res.status(200).send(processedUser);
+	}
+}
+
+export default UsersController;
